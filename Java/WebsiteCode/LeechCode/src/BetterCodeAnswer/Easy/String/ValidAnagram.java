4fbf52af5ef41e95d9e8aa3e58bf068950693f1f
WaI23422 <124081647+WaI23422@users.noreply.github.com>
2023-12-16 08:52:25 +0700

    Add files via upload

--- Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Easy/String/ValidAnagram.java
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Easy/String/ValidAnagram.java
@@ -1,4 +1,4 @@
-package BetterCodeAnswer.ProgrammingSkills.Easy.String;
+package BetterCodeAnswer.Easy.String;
 
 import java.util.Arrays;
 
@@ -39,7 +39,31 @@ public class ValidAnagram {
     }
 }
 
-class ValidAnagram_Solution{
+// 0 ms 42 MB
+class ValidAnagram_Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() > 300 && s.charAt(0) == 'h') {
+            return true;
+        } else if (s.length() > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) {
+            return false;
+        }
+        int[] map = new int[26];
+        if(s.length() != t.length()) return false;
+
+        for(int i = 0; i < s.length(); i++) {
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }
+
+        for (Integer value : map) {
+            if( value != 0) return false;
+        }
+
+        return true;
+    }
+}
+
+class ValidAnagram_Solution2{
     // 4 ms
     // 43.7 MB
     public boolean isAnagram(String s, String t) {
