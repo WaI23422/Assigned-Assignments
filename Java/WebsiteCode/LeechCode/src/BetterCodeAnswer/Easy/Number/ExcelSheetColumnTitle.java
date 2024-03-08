8859795695588cb9a6564f28aeffb33f7ac6835c
Bùi Tiến Thành <124081647+WaI23422@users.noreply.github.com>
2024-03-08 17:27:02 +0700

    Add files via upload

--- Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Easy/Number/ExcelSheetColumnTitle.java
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Easy/Number/ExcelSheetColumnTitle.java
@@ -16,9 +16,38 @@ public class ExcelSheetColumnTitle {
     }
 }
 
-// @see Easy.Number.ExcelSheetColumnTitle.java
+// 0 ms 40.78 MB
 class ExcelSheetColumnTitle_Solution {
     public String convertToTitle(int columnNumber) {
-        return "";
+        char arr[] = new char[27];
+        String str = "";
+
+        for(int i=1;i<arr.length;i++){
+            arr[i] = (char)(64+i);
+        }
+
+        if(columnNumber > 26){
+            while(columnNumber/26 >= 1){
+                 
+                if(columnNumber%26 <= 26){
+                    if(columnNumber%26 == 0){
+                        str = "Z"+str;
+                        columnNumber = columnNumber-1;
+                    }else {
+                        str = String.valueOf(arr[columnNumber%26])+str;
+                    }
+                }
+                if(columnNumber/26 <= 26){
+                    str = String.valueOf(arr[columnNumber/26])+str;
+                    break;
+                }else {
+                    columnNumber = columnNumber/26;
+                }
+            }
+        }else{
+            str = String.valueOf(arr[columnNumber]);
+        }
+
+        return str;
     }
 }
