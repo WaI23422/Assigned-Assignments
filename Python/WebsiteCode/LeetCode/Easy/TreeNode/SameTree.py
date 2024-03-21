6721a27571904c551e63448deeb0d33dbdb5c4a1
Bùi Tiến Thành <124081647+WaI23422@users.noreply.github.com>
2024-03-21 23:42:28 +0700

    Add files via upload

--- Python/WebsiteCode/LeetCode/Easy/TreeNode/SameTree.py
+++ Python/WebsiteCode/LeetCode/Easy/TreeNode/SameTree.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -16,3 +10,5 @@ class Solution(object):
         if not p or not q or p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
