be9d0037529faa913b58b34ed729a0869e5d722a
Bùi Tiến Thành <124081647+WaI23422@users.noreply.github.com>
2024-03-21 23:34:33 +0700

    Add files via upload

---
+++ Python/WebsiteCode/LeetCode/Easy/ListNode/ReverseLinkedList.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseList(self, head):
+        prev=None
+        cur=head
+        while cur:
+            nxt=cur.next
+            cur.next=prev
+            prev=cur
+            cur=nxt
+        head=prev
+        return head
